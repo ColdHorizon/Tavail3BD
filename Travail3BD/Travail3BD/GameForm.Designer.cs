277acf2c1c6aea95a0bc488497866bb9e85efa7a
JulienBoulanger <2436641@cegepba.qc.ca>
2026-04-29 16:57:19 -0400

    Projectionist + Upgrades
    YOU AINT NO TOJI

--- Travail3BD/Travail3BD/GameForm.Designer.cs
+++ Travail3BD/Travail3BD/GameForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace Travail3BD
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Travail3BD
 {
     partial class GameForm
     {
@@ -12,8 +15,6 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
 
         private void InitializeComponent()
         {
@@ -29,54 +30,42 @@
             textBoxUpgrades = new TextBox();
             buttonUpg = new Button();
             SuspendLayout();
-            // 
-            // labelPlayerHP
-            // 
+
             labelPlayerHP.AutoSize = true;
             labelPlayerHP.Location = new Point(30, 30);
             labelPlayerHP.Name = "labelPlayerHP";
             labelPlayerHP.Size = new Size(72, 20);
             labelPlayerHP.TabIndex = 0;
             labelPlayerHP.Text = "Player HP";
-            // 
-            // labelPlayerEnergy
-            // 
+
             labelPlayerEnergy.AutoSize = true;
             labelPlayerEnergy.Location = new Point(30, 60);
             labelPlayerEnergy.Name = "labelPlayerEnergy";
             labelPlayerEnergy.Size = new Size(98, 20);
             labelPlayerEnergy.TabIndex = 1;
             labelPlayerEnergy.Text = "Player Energy";
-            // 
-            // labelEnemyName
-            // 
+
             labelEnemyName.AutoSize = true;
             labelEnemyName.Location = new Point(400, 30);
             labelEnemyName.Name = "labelEnemyName";
             labelEnemyName.Size = new Size(97, 20);
             labelEnemyName.TabIndex = 2;
             labelEnemyName.Text = "Enemy Name";
-            // 
-            // labelEnemyHP
-            // 
+
             labelEnemyHP.AutoSize = true;
             labelEnemyHP.Location = new Point(400, 60);
             labelEnemyHP.Name = "labelEnemyHP";
             labelEnemyHP.Size = new Size(76, 20);
             labelEnemyHP.TabIndex = 3;
             labelEnemyHP.Text = "Enemy HP";
-            // 
-            // labelScore
-            // 
+
             labelScore.AutoSize = true;
             labelScore.Location = new Point(30, 90);
             labelScore.Name = "labelScore";
             labelScore.Size = new Size(46, 20);
             labelScore.TabIndex = 4;
             labelScore.Text = "Score";
-            // 
-            // buttonAtk1
-            // 
+
             buttonAtk1.Location = new Point(30, 154);
             buttonAtk1.Name = "buttonAtk1";
             buttonAtk1.Size = new Size(150, 40);
@@ -84,9 +73,7 @@
             buttonAtk1.Text = "Attack 1";
             buttonAtk1.UseVisualStyleBackColor = true;
             buttonAtk1.Click += button1_Click;
-            // 
-            // buttonAtk2
-            // 
+
             buttonAtk2.Location = new Point(30, 200);
             buttonAtk2.Name = "buttonAtk2";
             buttonAtk2.Size = new Size(150, 40);
@@ -94,9 +81,7 @@
             buttonAtk2.Text = "Attack 2";
             buttonAtk2.UseVisualStyleBackColor = true;
             buttonAtk2.Click += button2_Click;
-            // 
-            // buttonAtk3
-            // 
+
             buttonAtk3.Location = new Point(30, 250);
             buttonAtk3.Name = "buttonAtk3";
             buttonAtk3.Size = new Size(150, 40);
@@ -104,25 +89,19 @@
             buttonAtk3.Text = "Attack 3";
             buttonAtk3.UseVisualStyleBackColor = true;
             buttonAtk3.Click += button3_Click;
-            // 
-            // textBoxLog
-            // 
+
             textBoxLog.Location = new Point(220, 150);
             textBoxLog.Multiline = true;
             textBoxLog.Name = "textBoxLog";
             textBoxLog.ScrollBars = ScrollBars.Vertical;
             textBoxLog.Size = new Size(350, 250);
             textBoxLog.TabIndex = 8;
-            // 
-            // textBoxUpgrades
-            // 
+
             textBoxUpgrades.Location = new Point(41, 341);
             textBoxUpgrades.Name = "textBoxUpgrades";
             textBoxUpgrades.Size = new Size(125, 27);
             textBoxUpgrades.TabIndex = 9;
-            // 
-            // buttonUpg
-            // 
+
             buttonUpg.Location = new Point(30, 374);
             buttonUpg.Name = "buttonUpg";
             buttonUpg.Size = new Size(150, 40);
@@ -130,9 +109,7 @@
             buttonUpg.Text = "Upgrade";
             buttonUpg.UseVisualStyleBackColor = true;
             buttonUpg.Click += buttonUpg_Click;
-            // 
-            // GameForm
-            // 
+
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(600, 450);
@@ -149,12 +126,9 @@
             Controls.Add(labelPlayerHP);
             Name = "GameForm";
             Text = "GameForm";
-            Load += GameForm_Load_1;
             ResumeLayout(false);
             PerformLayout();
         }
-
-        #endregion
 
         private Label labelPlayerHP;
         private Label labelPlayerEnergy;
--- Travail3BD/Travail3BD/GameForm.cs
+++ Travail3BD/Travail3BD/GameForm.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
-using System.Collections;
 using System.Data;
 using System.Windows.Forms;
 
@@ -23,19 +22,25 @@ namespace Travail3BD
         int enemyAtk;
         int enemyDef;
 
+        int projectionCharges = 0;
+        bool upgradePending = false;
+
+        int focusBonus = 0;
+
         Random rng = new Random();
-
         DataTable abilityTable = new DataTable();
-
-
 
         public GameForm(int id)
         {
             InitializeComponent();
             playerId = id;
             this.Load += GameForm_Load;
+
             textBoxUpgrades.Enabled = false;
             buttonUpg.Enabled = false;
+
+            textBoxLog.ReadOnly = true;
+            textBoxLog.TabStop = false;
         }
 
         private void GameForm_Load(object sender, EventArgs e)
@@ -53,10 +58,13 @@ namespace Travail3BD
             using (SqlConnection c = new SqlConnection(conn))
             {
                 c.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "SELECT classes.hpBase, classes.atkBase, classes.defBase " +
+                    "FROM players " +
+                    "JOIN classes ON players.classId = classes.classId " +
+                    "WHERE players.playerId = @id", c);
 
-                SqlCommand cmd = new SqlCommand("SELECT hp, atk, def FROM players WHERE playerId = @id", c);
                 cmd.Parameters.AddWithValue("@id", playerId);
-
                 SqlDataReader r = cmd.ExecuteReader();
 
                 if (r.Read())
@@ -78,7 +86,6 @@ namespace Travail3BD
             using (SqlConnection c = new SqlConnection(conn))
             {
                 c.Open();
-
                 SqlDataAdapter da = new SqlDataAdapter(
                     "SELECT abilities.abilityName, abilities.abilityPower, abilities.abilityType " +
                     "FROM abilities " +
@@ -89,9 +96,20 @@ namespace Travail3BD
                 );
 
                 da.SelectCommand.Parameters.AddWithValue("@id", playerId);
-
                 da.Fill(abilityTable);
             }
+
+            DataRow basic = abilityTable.NewRow();
+            basic["abilityName"] = "Basic Attack";
+            basic["abilityPower"] = 0;
+            basic["abilityType"] = "basic";
+            abilityTable.Rows.InsertAt(basic, 0);
+
+            DataRow focus = abilityTable.NewRow();
+            focus["abilityName"] = "Focus";
+            focus["abilityPower"] = 0;
+            focus["abilityType"] = "focus";
+            abilityTable.Rows.InsertAt(focus, 1);
         }
 
         private void LoadEnemy()
@@ -101,7 +119,6 @@ namespace Travail3BD
             using (SqlConnection c = new SqlConnection(conn))
             {
                 c.Open();
-
                 SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM enemies", c);
                 DataTable dt = new DataTable();
                 da.Fill(dt);
@@ -132,27 +149,38 @@ namespace Travail3BD
 
             if (abilityTable.Rows.Count > 0)
                 buttonAtk1.Text = abilityTable.Rows[0]["abilityName"].ToString();
-            else
-                buttonAtk1.Text = "-";
-
             if (abilityTable.Rows.Count > 1)
                 buttonAtk2.Text = abilityTable.Rows[1]["abilityName"].ToString();
-            else
-                buttonAtk2.Text = "-";
-
             if (abilityTable.Rows.Count > 2)
                 buttonAtk3.Text = abilityTable.Rows[2]["abilityName"].ToString();
-            else
-                buttonAtk3.Text = "-";
         }
 
         private void PlayerAttack(int power, int cost, string abilityName, string abilityType)
         {
-            if (playerEnergy < cost)
+            if (abilityType == "focus")
             {
-                MessageBox.Show("Pas assez d'énergie");
+                int restore = 1 + focusBonus;
+                playerEnergy += restore;
+                if (playerEnergy > 3) playerEnergy = 3;
+
+                textBoxLog.AppendText("Tu te concentres et récupères " + restore + " énergie.\r\n");
+                EnemyTurn();
+                UpdateUI();
                 return;
             }
+
+            if (abilityType == "basic")
+            {
+                power = playerAtk * 2;
+            }
+
+            if (abilityType != "basic" && abilityType != "focus")
+                cost = 1;
+            else
+                cost = 0;
+
+            if (playerEnergy < cost)
+                return;
 
             playerEnergy -= cost;
 
@@ -163,6 +191,22 @@ namespace Travail3BD
                     power *= 3;
                 else if (roll < 25)
                     power *= 2;
+            }
+
+            if (abilityType == "projection")
+            {
+                projectionCharges++;
+                textBoxLog.AppendText("Projection charge (" + projectionCharges + "/3).\r\n");
+
+                if (projectionCharges >= 3)
+                {
+                    power *= 3;
+                    playerHp += 1;
+                    if (playerHp > playerMaxHp) playerHp = playerMaxHp;
+
+                    textBoxLog.AppendText("Projection burst! Triple dégâts +1 HP.\r\n");
+                    projectionCharges = 0;
+                }
             }
 
             int dmg = power + playerAtk - enemyDef;
@@ -182,60 +226,56 @@ namespace Travail3BD
             EnemyTurn();
             UpdateUI();
         }
+
         private void Upgrade()
         {
-            
+            if (upgradePending)
+                return;
+
+            upgradePending = true;
 
             string conn = @"Server=localhost;Database=FurryRPG;Trusted_Connection=True;TrustServerCertificate=True";
-            string info = "select upgradeName,upgradeValue,upgradetype from upgrades where upgradeid = @id";
-
-
 
             using (SqlConnection c = new SqlConnection(conn))
             {
                 c.Open();
-                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM upgrades", c);
+                SqlDataAdapter da = new SqlDataAdapter("SELECT upgradeId, upgradeName, upgradeValue FROM upgrades", c);
                 DataTable table = new DataTable();
                 da.Fill(table);
-                int count = table.Rows.Count;
-                textBoxLog.AppendText("Choose an upgrade ");
+
+                textBoxLog.AppendText("Choose an upgrade:\r\n");
+
                 textBoxUpgrades.Enabled = true;
                 buttonAtk1.Enabled = false;
                 buttonAtk2.Enabled = false;
                 buttonAtk3.Enabled = false;
                 buttonUpg.Enabled = true;
-                for (int i = 1; i <= count; i++)
-                {
 
-                    SqlCommand infoUpgrades = new SqlCommand(info, c);
-                    infoUpgrades.Parameters.AddWithValue("@id", i);
-                    using (SqlDataReader reader = infoUpgrades.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            string Name = reader["upgradeName"].ToString();
-                            string Value = reader["upgradeValue"].ToString();
-                            string Type = reader["upgradetype"].ToString();
-                            textBoxLog.AppendText("" + Name + " Value  " + Value + " upgradetype " + Type + ". ");
-                        }
-                    }
+                foreach (DataRow row in table.Rows)
+                {
+                    string id = row["upgradeId"].ToString();
+                    string name = row["upgradeName"].ToString();
+                    string value = row["upgradeValue"].ToString();
+
+                    textBoxLog.AppendText(id + ": " + name + " (" + value + ")\r\n");
                 }
             }
-
         }
+
         private void EnemyTurn()
         {
-            int dmg = enemyAtk - playerDef;
-            if (dmg < 0) dmg = 0;
+            float reduction = playerDef / 100f;
+            if (reduction > 0.90f) reduction = 0.90f;
+
+            int dmg = (int)Math.Floor(enemyAtk * (1 - reduction));
+            if (dmg < 1) dmg = 1;
 
             playerHp -= dmg;
 
             textBoxLog.AppendText(enemyName + " attaque et inflige " + dmg + " dégâts.\r\n");
 
             if (playerHp <= 0)
-            {
                 GameOver();
-            }
         }
 
         private void EnemyDefeated()
@@ -251,7 +291,6 @@ namespace Travail3BD
             playerHp += heal;
             if (playerHp > playerMaxHp) playerHp = playerMaxHp;
 
-            
             UpdateUI();
         }
 
@@ -263,60 +302,69 @@ namespace Travail3BD
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (abilityTable.Rows.Count > 0)
-            {
-                string name = abilityTable.Rows[0]["abilityName"].ToString();
-                int power = (int)abilityTable.Rows[0]["abilityPower"];
-                string type = abilityTable.Rows[0]["abilityType"].ToString();
-                PlayerAttack(power, 0, name, type);
-            }
+            string name = abilityTable.Rows[0]["abilityName"].ToString();
+            int power = (int)abilityTable.Rows[0]["abilityPower"];
+            string type = abilityTable.Rows[0]["abilityType"].ToString();
+            PlayerAttack(power, 0, name, type);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (abilityTable.Rows.Count > 1)
-            {
-                string name = abilityTable.Rows[1]["abilityName"].ToString();
-                int power = (int)abilityTable.Rows[1]["abilityPower"];
-                string type = abilityTable.Rows[1]["abilityType"].ToString();
-                PlayerAttack(power, 1, name, type);
-            }
+            string name = abilityTable.Rows[1]["abilityName"].ToString();
+            int power = (int)abilityTable.Rows[1]["abilityPower"];
+            string type = abilityTable.Rows[1]["abilityType"].ToString();
+            PlayerAttack(power, 1, name, type);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (abilityTable.Rows.Count > 2)
-            {
-                string name = abilityTable.Rows[2]["abilityName"].ToString();
-                int power = (int)abilityTable.Rows[2]["abilityPower"];
-                string type = abilityTable.Rows[2]["abilityType"].ToString();
-                PlayerAttack(power, 0, name, type);
-            }
-        }
-
-        private void GameForm_Load_1(object sender, EventArgs e)
-        {
-
+            string name = abilityTable.Rows[2]["abilityName"].ToString();
+            int power = (int)abilityTable.Rows[2]["abilityPower"];
+            string type = abilityTable.Rows[2]["abilityType"].ToString();
+            PlayerAttack(power, 0, name, type);
         }
 
         private void buttonUpg_Click(object sender, EventArgs e)
         {
-            string conn = @"Server=localhost;Database=FurryRPG;Trusted_Connection=True;TrustServerCertificate=True";
-            if (textBoxUpgrades.Text.Trim() == "")
+            if (!int.TryParse(textBoxUpgrades.Text, out int choice))
+                return;
+
+            if (choice == 1)
             {
-                MessageBox.Show("Need to add a value in the upgrades");
+                playerMaxHp += 2;
+                playerHp += 2;
+                textBoxLog.AppendText("HP increased.\r\n");
+            }
+            else if (choice == 2)
+            {
+                playerAtk += 1;
+                textBoxLog.AppendText("ATK increased.\r\n");
+            }
+            else if (choice == 3)
+            {
+                playerDef += 1;
+                textBoxLog.AppendText("DEF increased.\r\n");
+            }
+            else if (choice == 4)
+            {
+                focusBonus += 1;
+                textBoxLog.AppendText("Focus restores +1 more energy.\r\n");
             }
             else
-            {
-                textBoxUpgrades.Enabled = false;
-                buttonAtk1.Enabled = true;
-                buttonAtk2.Enabled = true;
-                buttonAtk3.Enabled = true;
-                buttonUpg.Enabled = false;
-                LoadEnemy();
-                UpdateUI();
-            }
-           
+                return;
+
+            upgradePending = false;
+
+            textBoxUpgrades.Enabled = false;
+            buttonAtk1.Enabled = true;
+            buttonAtk2.Enabled = true;
+            buttonAtk3.Enabled = true;
+            buttonUpg.Enabled = false;
+
+            textBoxUpgrades.Text = "";
+
+            LoadEnemy();
+            UpdateUI();
         }
     }
 }
